9b6ddfea53caedfd087b00453ec1636674fa8572
Abdulhamid109 <abdulhamidpatel109@gmail.com>
2026-07-12 08:29:41 +0530

    minor code changes

--- utils/inngest/functions/keep-alive.js
+++ utils/inngest/functions/keep-alive.js
@@ -4,7 +4,7 @@ import { inngestClient } from "../inngestClient";
 export const KeepAlive = inngestClient.createFunction(
     {id:"Keep-me-alive",triggers:{'cron':"*/3 * * * *"}},
     async({event,step})=>{
-        const res = await step.run('keep-me-alive',()=>{
+        const res = await step.run('keep-me-alive',async()=>{
             await fetch(`https://queueless-backend-app.onrender.com/customer/keep-me-alive`,{
                 method:"GET"
             });
