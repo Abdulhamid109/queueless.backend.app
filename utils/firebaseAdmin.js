edddb200c9f3119e5759a673d7c0a87b9dbe7625
Abdulhamid109 <abdulhamidpatel109@gmail.com>
2026-07-14 19:41:40 +0530

    trying to check the chnages

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -3,7 +3,7 @@ import {readFileSync} from "fs";
 
 
 const serviceAccount = JSON.parse(
-    readFileSync(new URL("/queueless-fcm-firebase-adminsdk-fbsvc-34f749eadb.json", import.meta.url))
+    readFileSync(new URL("queueless-fcm-firebase-adminsdk-fbsvc-34f749eadb.json", import.meta.url))
 );
 
 admin.initializeApp({
